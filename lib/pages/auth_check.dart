1d554efb75058396526bff523479bc2c5ad56cbd
Dal <brardal@hotmail.com>
2025-11-09 08:42:21 -0700

    - fixed bug where App was letting deleted/signed-out user still get to the home screen instead of signing them out

--- lib/pages/auth_check.dart
+++ lib/pages/auth_check.dart
@@ -1,6 +1,7 @@
 import 'package:bingio/pages/login_page.dart';
 import 'package:bingio/pages/register_page.dart';
 import 'package:bingio/pages/verify_email_page.dart';
+import 'package:bingio/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +19,23 @@ class _AuthCheckState extends State<AuthCheck> {
     setState(() {
       showRegisterPage = !showRegisterPage;
     });
+  }
+
+  Future<void> _checkUserValidity() async {
+    final user = AuthService().getCurrentUser();
+    if (user != null) {
+      try {
+        await user.getIdToken(true);
+      } catch(e) {
+        await AuthService().logOut();
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkUserValidity();
   }
 
   @override
